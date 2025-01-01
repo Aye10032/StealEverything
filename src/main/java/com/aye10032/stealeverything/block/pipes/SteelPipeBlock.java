45d525b8cd205ac894d5953212edee37cc6c2410
Aye10032 <tc_aye@outlook.com>
2025-01-01 23:30:28 +0800

    管道材质

---
+++ src/main/java/com/aye10032/stealeverything/block/pipes/SteelPipeBlock.java
@@ -0,0 +1,16 @@
+package com.aye10032.stealeverything.block.pipes;
+
+import com.simibubi.create.content.fluids.pipes.FluidPipeBlock;
+
+/**
+ * @program: stealeverything
+ * @description: 钢制流体管道
+ * @author: Aye10032
+ * @create: 2025-01-01 23:27
+ **/
+
+public class SteelPipeBlock extends FluidPipeBlock {
+    public SteelPipeBlock(Properties properties) {
+        super(properties);
+    }
+}
